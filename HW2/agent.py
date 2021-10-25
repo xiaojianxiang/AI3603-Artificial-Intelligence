d991fb121752dd8dadbd92e9cfe8222cb07eca2d
Xiangyu Zhao <843080788@qq.com>
2021-10-25 10:53:59 +0800

    Cliff Walking SARSA Finished.

--- HW2/agent.py
+++ HW2/agent.py
@@ -19,21 +19,66 @@ You need to implement the Q-learning agent, Sarsa agent and Dyna-Q agent in this
 class SarsaAgent(object):
     ##### START CODING HERE #####
 
-    def __init__(self, all_actions):
+    def __init__(self, num_observations, num_actions, all_actions, learning_rate = 0.05, gamma = 0.9, epsilon = 1):
         """initialize the agent. Maybe more function inputs are needed."""
         self.all_actions = all_actions
-        self.epsilon = 1.0
+        self.lr = learning_rate
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.Q = np.zeros((num_observations, num_actions))
 
     def choose_action(self, observation):
         """choose action with epsilon-greedy algorithm."""
-        action = np.random.choice(self.all_actions)
+        if np.random.rand() <= self.epsilon:
+            # epsilon randomly choose action
+            action = np.random.choice(self.all_actions)
+        else:
+            # greedily choose action
+            action = self.predict(observation)
         return action
     
-    def learn(self):
-        """learn from experience"""
-        time.sleep(0.5)
+    def learn(self, observation, action, reward, observation_, action_, done):
+        """
+            learn from experience
+            update the Q-table
+        """
+        # time.sleep(0.5)
+
+        predict_Q = self.Q[observation, action]
+        if done:
+            target_Q = reward
+        else:
+            target_Q = reward + self.gamma * self.Q[observation_, action_]
+        self.Q[observation, action] += self.lr * (target_Q - predict_Q)
+
         print("[INFO] The learning process complete. (ﾉ｀⊿´)ﾉ")
         return True
+    
+    def predict(self, observation):
+        Q_list = self.Q[observation, :]
+        if np.count_nonzero(Q_list) == 0:
+            action = np.random.choice(self.all_actions)
+        else:
+            action = np.argmax(Q_list)
+        return action
+
+    def save(self, npy_file = './Q_Table_SARSA.npy'):
+        '''Save the learning process'''
+        np.save(npy_file, self.Q)
+        print(npy_file + ' saved.')
+
+    def restore(self, npy_file='./Q_Table_SARSA.npy'):
+        '''Load the learning process'''
+        self.Q = np.load(npy_file)
+        print(npy_file + ' loaded.')
+    
+    def set_epsilon(self, epsilon = 0.1):
+        self.epsilon = epsilon
+
+    def your_function(self, params):
+        """You can add other functions as you wish."""
+        do_something = True
+        return None
     
     def your_function(self, params):
         """You can add other functions as you wish."""
--- HW2/cliff_walk_sarsa.py
+++ HW2/cliff_walk_sarsa.py
@@ -17,6 +17,8 @@ env = CliffWalk()
 # get the size of action space 
 num_actions = env.action_space.n
 all_actions = np.arange(num_actions)
+# get the size of observation space 
+num_observations = env.observation_space.n
 # set random seed and make the result reproducible
 RANDOM_SEED = 0
 env.seed(RANDOM_SEED)
@@ -26,7 +28,14 @@ np.random.seed(RANDOM_SEED)
 ####### START CODING HERE #######
 
 # construct the intelligent agent.
-agent = SarsaAgent(all_actions) 
+agent = SarsaAgent(num_observations, num_actions, all_actions) 
+
+# agent.restore('/Users/xiaojian_xiang/Projects/AI3606/HW2/SARSA/Q_Table_SARSA.npy')
+
+# The scope of the episode reward
+episode_rewards = []
+episode_epsilons = []
+epsilon = 1.0
 
 # start training
 for episode in range(1000):
@@ -37,21 +46,48 @@ for episode in range(1000):
     # render env. You can comment all render() to turn off the GUI to accelerate training.
     env.render()
     # agent interacts with the environment
+    episode_epsilons.append(epsilon)
+    agent.set_epsilon(epsilon)
+    # Set epsilon = epsilon(k-1) * 0.95
+    a = agent.choose_action(s)
+    # choose an action
     for iter in range(500):
-        # choose an action
-        a = agent.choose_action(s)
         s_, r, isdone, info = env.step(a)
+        a_ = agent.choose_action(s_)
         env.render()
         # update the episode reward
         episode_reward += r
         print(f"{s} {a} {s_} {r} {isdone}")
         # agent learns from experience
-        agent.learn()
+        agent.learn(s, a, r, s_, a_, isdone)
         s = s_
+        a = a_
         if isdone:
-            time.sleep(0.5)
+            # time.sleep(0.5)
             break
-    print('episode:', episode, 'episode_reward:', episode_reward, 'epsilon:', agent.epsilon)  
+    print('episode:', episode, 'episode_reward:', episode_reward, 'epsilon:', agent.epsilon)
+    episode_rewards.append(episode_reward)
+    epsilon = epsilon * 0.95
+    agent.save('/Users/xiaojian_xiang/Projects/AI3606/HW2/SARSA/Q_Table_SARSA.npy')
+
+# Save the episode reward
+np.save('/Users/xiaojian_xiang/Projects/AI3606/HW2/SARSA/reward.npy', episode_rewards)
+# Save the episode epsilon
+np.save('/Users/xiaojian_xiang/Projects/AI3606/HW2/SARSA/epsilon.npy', episode_epsilons)
+
+import matplotlib.pyplot as plt
+plt.figure(1)
+plt.plot(episode_rewards)
+plt.xlabel('Episode')
+plt.ylabel('Episode Rewards')
+plt.show()
+
+plt.figure(2)
+plt.plot(episode_epsilons)
+plt.xlabel('Episode')
+plt.ylabel('Episode Epsilons')
+plt.show()
+
 print('\ntraining over\n')   
 
 # close the render window after training.
